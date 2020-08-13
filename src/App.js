a992d1cbd28031c42dd9173115768e7d1c4c5c65
Alex Williams <alexjwilliams1@gmail.com>
2020-08-13 15:53:40 -0700

    stretch, removed button enabled

--- src/App.js
+++ src/App.js
@@ -18,10 +18,16 @@ function App() {
 		setCart([...cart, item])
 	};
 
+	const removeItem = selectedItemId => {
+		const newCart = cart.filter( cartItem => selectedItemId !== cartItem.id)
+
+		setCart(newCart)
+	}
+
 	return (
 		<div className="App">
 			<ProductContext.Provider value={{ products, addItem }}>
-				<CartContext.Provider value = {cart}>
+				<CartContext.Provider value = {{cart, removeItem}}>
 
 				<Navigation />
 
